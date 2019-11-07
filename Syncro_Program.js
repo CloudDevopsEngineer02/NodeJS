af8bf688c1f437b3f0637b87f3c5b4b59b43f445
CloudDevopsEngineer02 <cloud.devops.02@gmail.com>
2019-11-07 06:34:28 +0000

    Syncronous Function programs

--- Syncro_Program.js
+++ Syncro_Program.js
@@ -1,8 +1,23 @@
-function add(a,b)
+//function mul(a,b)
+//{
+// return(a-b);
+//}
+
+//console.log(mul(5,6));
+
+
+//This is a syncronous function
+//function div(a,b)
 {
- return(a+b);
+//return(a/b);
 }
+//console.log(div(10,100));
+////O/P: 0.1
 
-console.log(add(5,6));
+//This is a syncronous function 2nd Example
 
-
+function div(a,b)
+{
+return(a*b);
+}
+console.log(div(10,100));
