a06b82c7c6cb187ea81ce645e098f0497e52b738
CloudDevopsEngineer02 <cloud.devops.02@gmail.com>
2019-11-08 06:33:08 +0000

    My first deployment

---
+++ My-Apps/index.js
@@ -0,0 +1,18 @@
+const logger = require('./logger');
+const underscore = require("underscore");
+logger.log("hELLO wORLD");
+
+logger.log("Hellow World from my first App");
+console.log(underscore.first([202,2,133,202]));
+//should not keep 0 infront of numarics if you do you will get 130 as output.
+
+//const logger = require('./logger');
+//const underscore = require("underscore");
+logger.log("My First Application Developement");
+console.log(underscore.difference([1, 2, 3, 4, 5], [5, 2, 10]));
+//output should be  [1, 3, 4]
+
+
+
+
+
